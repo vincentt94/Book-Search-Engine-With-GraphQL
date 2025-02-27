18eebb5c6a399c741db5ae2d6989718fdf89e9aa
vincentt94 <vincent.h.thao@gmail.com>
2025-02-27 13:48:33 -0600

    updated schemas folder with index.ts, reseolvers, and typeDefs.  Updated server.ts on server as well.

---
+++ server/src/schemas/typeDefs.ts
@@ -0,0 +1,45 @@
+const typeDefs = `
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: String!
+    user: User!
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+
+  input BookInput {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+  }
+`;
+
+  export default typeDefs;
