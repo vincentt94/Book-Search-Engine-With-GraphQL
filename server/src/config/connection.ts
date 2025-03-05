9cf56af057e9a223e50d393bb447c8436156a61f
vincentt94 <vincent.h.thao@gmail.com>
2025-03-04 19:23:26 -0600

    resolved issues with deployment

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
-const MONGODB_URI = process.env.MONGODB_URI || '';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks';
 const db = async (): Promise<typeof mongoose.connection> => {
   try {
     await mongoose.connect(MONGODB_URI);
--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,10 @@ import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/serve
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './utils/auth.js';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 const server = new ApolloServer({
@@ -31,10 +35,11 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
+    console.log('hi');
 
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
 
